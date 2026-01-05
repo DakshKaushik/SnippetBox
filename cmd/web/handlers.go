805fe22c21937a897ca9e33cb502a4b1d740ce4b
DakshKaushik <kaushikdaksh03@gmail.com>
2026-01-05 21:15:19 +0530

    Project Structure

---
+++ cmd/web/handlers.go
@@ -0,0 +1,45 @@
+package main
+import (
+	"net/http"
+	"fmt"
+	"strconv"
+	"html/template"
+)
+func(app *application) home(w http.ResponseWriter,r *http.Request){
+	
+	if r.URL.Path!="/"{
+		http.NotFound(w,r)
+		return
+	}
+	files:=[]string{
+		"./ui/html/pages/home.html",
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+	}
+	ts,err:=template.ParseFiles(files...)
+	if err!=nil{
+		app.errorLog.Println(err.Error())
+		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+		return
+	}
+	err=ts.ExecuteTemplate(w,"base",nil)
+	if err!=nil{
+		app.errorLog.Println(err.Error())
+		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+	}
+}
+func (app *application)snippetView(w http.ResponseWriter,r *http.Request){
+	id,err:=strconv.Atoi(r.URL.Query().Get("id"))
+	if err!=nil||id<1{
+		http.NotFound(w,r)
+		return;
+	}
+	fmt.Fprintf(w,"Display a specific snippet with ID %d",id)
+}
+func (app *application)snippetCreate(w http.ResponseWriter, r* http.Request)  {
+	if r.Method!=http.MethodPost{
+		http.Error(w,"Method Not Allow",http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new Snippet..."))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.enzc.dev
+
+go 1.25.5
