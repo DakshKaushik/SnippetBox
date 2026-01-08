5cbbe83b835ee257921a4d8ae4731ff66f127d7f
DakshKaushik <kaushikdaksh03@gmail.com>
2026-01-08 21:02:44 +0530

    adding template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,80 +1,79 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
-	"errors"
 
 	"snippetbox.enzc.dev/internal/models"
 )
-func(app *application) home(w http.ResponseWriter,r *http.Request){
-	
-	if r.URL.Path!="/"{
-		http.NotFound(w,r)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
 		return
 	}
 
-	snippets,err:=app.snippets.Latest()
-	if err!=nil{
-		app.serverError(w,err)
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
 	}
 
-	for _,snippet := range snippets{
-		fmt.Fprintf(w,"%+v\n",snippet)
-	}
-
-	files:=[]string{
-		"./ui/html/pages/home.html",
-		"./ui/html/base.html",
-		"./ui/html/partials/nav.html",
-	}
-	ts,err:=template.ParseFiles(files...)
-	if err!=nil{
-		app.serverError(w,err)
-		return
-	}
-	err=ts.ExecuteTemplate(w,"base",nil)
-	if err!=nil{
-		app.serverError(w,err)
-		return
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
 }
-func (app *application)snippetView(w http.ResponseWriter,r *http.Request){
-	id,err:=strconv.Atoi(r.URL.Query().Get("id"))
-	if err!=nil||id<1{
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
-	snippet,err:=app.snippets.Get(id)
 
-	if err!=nil{
-		if errors.Is(err,models.ErrNoRecord){
+	snippet, err := app.snippets.Get(id)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
 			app.notFound(w)
-		}else{
-			app.serverError(w,err)
+		} else {
+			app.serverError(w, err)
 		}
 		return
 	}
-	fmt.Fprintf(w,"%+v",snippet)
-}
-func (app *application)snippetCreate(w http.ResponseWriter, r* http.Request)  {
-	if r.Method!=http.MethodPost{
-		w.Header().Set("Allow",http.MethodPost)
-		app.clientError(w,http.StatusMethodNotAllowed)
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/view.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
 		return
 	}
-	title :="O snail"
-	content :="O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	err = ts.ExecuteTemplate(w, "base", snippet)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+}
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := 7
 
-	id,err:=app.snippets.Insert(title,content,expires)
-	if err!=nil{
-		app.serverError(w,err)
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
 		return
 	}
-	http.Redirect(w,r,fmt.Sprintf("/snippet/view?id=%d", id),http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 
 }
-
